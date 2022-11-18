c44b66aa551db46af0c6b64406d33232a018cc9c
Muhammad Nur Fauzi <fauzi.ozi.mnf@gmail.com>
2022-11-18 17:31:09 +0700

    Implement register and connecting it to backend

--- projects/mainarea/src/app/pages/sign-up/sign-up.component.ts
+++ projects/mainarea/src/app/pages/sign-up/sign-up.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { MenuItem } from 'primeng/api';
+import { Subscription } from "rxjs";
+import { SignUpService } from "../../service/sign-up.service";
+import { UserService } from "../../service/user.service";
 
 
 @Component({
     selector: "sign-up",
     templateUrl: "./sign-up.component.html"
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
 
     signUp = true
     accountDtl = false
@@ -14,7 +18,30 @@ export class SignUpComponent implements OnInit {
 
     items: MenuItem[] = []
 
+    registerForm = this.fb.group({
+        fullname: ['', [Validators.required, Validators.maxLength(50)]],
+        email: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
+        password: ['', [Validators.required]],
+        company: ['', [Validators.required]],
+        industry: ['', [Validators.required]],
+        position: ['', [Validators.required]]
+    })
+
+    verificationCode :any = this.fb.group({
+        verificationCode: ['', [Validators.required]],
+    })
+
+
+    private sendVerificationSubscription?: Subscription
+    private verificateCodeSubscription?: Subscription
+    private registerSubscription?: Subscription
+
+    constructor(private fb: FormBuilder, private signUpService: SignUpService,
+        private userService : UserService) { }
+
     ngOnInit(): void {
+
+
         this.items = [
             { label: "Sign Up" },
             { label: "Account Detail" },
@@ -23,10 +50,10 @@ export class SignUpComponent implements OnInit {
     }
 
     industries: any = [
-        { name: "ddddd" },
-        { name: "ddddd" },
-        { name: "ddddd" },
-        { name: "ddddd" }
+        { name: "1" },
+        { name: "2" },
+        { name: "3" },
+        { name: "4" }
     ]
 
     positions: any = [
@@ -45,5 +72,23 @@ export class SignUpComponent implements OnInit {
         this.accountDtl = false
         this.signUp = false
         this.verification = true
+        this.sendVerificationSubscription = this.signUpService.sendVerification(this.registerForm.value.email).subscribe(() => {
+
+        })
+    }
+
+    clickVerify() {
+        this.verificationCode.addControl('email', this.fb.control(this.registerForm.value.email, [Validators.required]))
+        this.verificateCodeSubscription = this.signUpService.verificateCode(this.verificationCode.value).subscribe(u=>{
+            if(u){
+                this.registerSubscription = this.userService.register(this.registerForm.value).subscribe(()=>{})
+            }
+        })
+    }
+
+    ngOnDestroy(): void {
+        this.sendVerificationSubscription?.unsubscribe()
+        this.verificateCodeSubscription?.unsubscribe()
+        this.registerSubscription?.unsubscribe()
     }
 }

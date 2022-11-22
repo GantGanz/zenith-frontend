7b5333a0b970783f7a5ad234ba5303199d757a87
Muhammad Nur Fauzi <fauzi.ozi.mnf@gmail.com>
2022-11-22 11:23:23 +0700

    fix user list

--- projects/adminarea/src/app/pages/master-user-admin/user-list/user-list.component.ts
+++ projects/adminarea/src/app/pages/master-user-admin/user-list/user-list.component.ts
@@ -16,8 +16,7 @@ export class UserListComponent implements OnInit, OnDestroy{
     usersRes!: UsersRes
     
     rows = 10;
-    
-    users: any[] = []
+    first= 0;
 
     pagination ={
         limit:0,
@@ -29,22 +28,8 @@ export class UserListComponent implements OnInit, OnDestroy{
     constructor(private userService: UserService){}
   
     ngOnInit(): void {
-        this.pagination.limit = this.rows
-        this.pagination.offset = this.first
-        this.usersSubscription = this.userService.getAll(this.pagination).subscribe(result=>{
+        this.usersSubscription = this.userService.getAll().subscribe(result=>{
             this.usersRes = result
-            for(let i=0;i<this.usersRes.data.length;i++){
-                this.users.push({
-                    id: this.usersRes.data[i].id,
-                    name: this.usersRes.data[i].fullname,
-                    email: this.usersRes.data[i].email,
-                    company: this.usersRes.data[i].company,
-                    industry: this.usersRes.data[i].industryName,
-                    position: this.usersRes.data[i].positionName,
-                    role: this.usersRes.data[i].roleName,
-                    file: this.usersRes.data[i].fileId
-                })
-            }
         })
     }
 

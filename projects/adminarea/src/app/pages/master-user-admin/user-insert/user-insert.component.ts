263b61f4ff8593d59efdd40cfd8ed64653ce2e37
Muhammad Nur Fauzi <fauzi.ozi.mnf@gmail.com>
2022-11-22 11:11:25 +0700

    Fix user list view and add admin registration feature

--- projects/adminarea/src/app/pages/master-user-admin/user-insert/user-insert.component.ts
+++ projects/adminarea/src/app/pages/master-user-admin/user-insert/user-insert.component.ts
@@ -1,20 +1,69 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { UserRes } from "projects/interface/user/user-res";
+import { IndustryService } from "projects/mainarea/src/app/service/industry.service";
+import { PositionService } from "projects/mainarea/src/app/service/position.service";
+import { UserService } from "projects/mainarea/src/app/service/user.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: "user-insert",
     templateUrl: "./user-insert.component.html"
 })
-export class UserInsertComponent {
-    industries: any = [
-        { name: "1" },
-        { name: "2" },
-        { name: "3" },
-        { name: "4" }
-    ]
-    positions: any = [
-        { name: "1" },
-        { name: "2" },
-        { name: "3" },
-        { name: "4" }
-    ]
+export class UserInsertComponent implements OnInit, OnDestroy {
+
+    userInsertForm = this.fb.group({
+        id: ['', [Validators.required]],
+        fullname: ['', [Validators.required, Validators.maxLength(50)]],
+        email: ['', [Validators.email, Validators.required, Validators.maxLength(50)]],
+        company: ['', [Validators.required]],
+        positionId: ['', [Validators.required]],
+        industryId: ['', [Validators.required]],
+    })
+
+    industries: any = []
+    positions: any = []
+
+    private industrySubscription?: Subscription
+    private userInsertSubscription?: Subscription
+    private positionSubscription?: Subscription
+
+    constructor(private industryService: IndustryService, private positionService: PositionService,
+        private userService: UserService, private fb: FormBuilder, private router:Router) { }
+
+    ngOnInit(): void {
+        this.industrySubscription = this.industryService.getAll().subscribe(industry => {
+            for (let i = 0; i < industry.data.length; i++) {
+                this.industries.push({
+                    name: industry.data[i].industryName,
+                    code: industry.data[i].industryCode,
+                    id: industry.data[i].id
+                })
+            }
+        })
+        this.positionSubscription = this.positionService.getAll().subscribe(result=>{
+            for(let i=0;i<result.data.length;i++){
+                this.positions.push({
+                    name: result.data[i].positionName,
+                    code: result.data[i].positionCode,
+                    id: result.data[i].id
+                })
+            }
+        })
+    }
+
+    clickInsert(){
+        console.log(this.userInsertForm.value);
+        
+        this.userInsertSubscription = this.userService.insert(this.userInsertForm.value).subscribe(()=>{
+            this.router.navigateByUrl('/users/list')
+        })
+    }
+
+    ngOnDestroy(): void {
+        this.industrySubscription?.unsubscribe()
+        this.positionSubscription?.unsubscribe()
+        this.userInsertSubscription?.unsubscribe()
+    }
 }
--- projects/mainarea/src/app/service/user.service.ts
+++ projects/mainarea/src/app/service/user.service.ts
@@ -1,21 +1,30 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { InsertRes } from "projects/interface/insert-res";
+import { UserRes } from "projects/interface/user/user-res";
 import { UsersRes } from "projects/interface/user/users-res";
 import { Observable } from "rxjs";
 import { BASE_URL } from "../constant/base.url";
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-export class UserService{
-    constructor(private http:HttpClient){}
+export class UserService {
+    constructor(private http: HttpClient) { }
 
-    register(data:any):Observable<InsertRes>{
-        return this.http.post<InsertRes>(`${BASE_URL.LOCALHOST}/users/register`,data)
+    register(data: any): Observable<InsertRes> {
+        return this.http.post<InsertRes>(`${BASE_URL.LOCALHOST}/users/register`, data)
     }
 
-    getAll(): Observable<UsersRes>{
-        return this.http.get<UsersRes>(`${BASE_URL.LOCALHOST}/users`)
+    getAll(data: any): Observable<UsersRes> {
+        return this.http.get<UsersRes>(`${BASE_URL.LOCALHOST}/users?offset=${data.offset}&limit=100`)
+    }
+
+    insert(data: any): Observable<InsertRes>{
+        return this.http.post<InsertRes>(`${BASE_URL.LOCALHOST}/users`,data)
+    }
+
+    getById(id: string): Observable<UserRes>{
+        return this.http.get<UserRes>(`${BASE_URL.LOCALHOST}/users/${id}`)
     }
 }

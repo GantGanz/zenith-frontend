1b9b2241eb3ac09582e3736532b99a9299017057
Ince Clara Mituduan <inceclaramituduan@gmail.com>
2022-11-23 10:41:48 +0700

    Update profile page (admin)

--- projects/adminarea/src/app/pages/profile/view-profile/profile-view.component.ts
+++ projects/adminarea/src/app/pages/profile/view-profile/profile-view.component.ts
@@ -1,7 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "profile-view",
     templateUrl: "./profile-view.component.html"
 })
-export class ProfileViewComponent { }
+export class ProfileViewComponent implements OnInit {
+
+    editProfileSuperAdmin = false
+    editProfileAdmin = false
+    changePasswordSuperAdmin = false
+    changePasswordAdmin = false
+
+    constructor(private router: Router) { }
+
+    ngOnInit(): void {
+        if (this.router.url == "/super-admin/profiles/view") {
+            this.editProfileSuperAdmin = true
+            this.changePasswordSuperAdmin = true
+        } else if (this.router.url == "/admin/profiles/view") {
+            this.editProfileAdmin = true
+            this.changePasswordAdmin = true
+        }
+    }
+}

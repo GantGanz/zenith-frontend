fbab93d767f7ee1e7a655d607b44a58762657944
Muhammad Nur Fauzi <fauzi.ozi.mnf@gmail.com>
2022-11-24 09:11:30 +0700

    update industry crud and user list

--- projects/adminarea/src/app/pages/master-industry/industry-update/industry-update.component.ts
+++ projects/adminarea/src/app/pages/master-industry/industry-update/industry-update.component.ts
@@ -1,7 +1,52 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { IndustryService } from "projects/mainarea/src/app/service/industry.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: "industry-update",
     templateUrl: "./industry-update.component.html"
 })
-export class IndustryUpdateComponent { }
+export class IndustryUpdateComponent implements OnInit, OnDestroy {
+
+    private industrySubscription?: Subscription
+    private industryUpdateSubscription?: Subscription
+    private paramSubscription?: Subscription
+
+
+    industryForm = this.fb.group({
+        id: ['', [Validators.required]],
+        industryCode: ['', [Validators.required, Validators.maxLength(5)]],
+        industryName: ['', [Validators.required, Validators.maxLength(50)]],
+        isActive: [true, [Validators.required]],
+        version: [0, [Validators.required]]
+    })
+
+    constructor(private industryService: IndustryService, private fb: FormBuilder,
+        private active: ActivatedRoute) { }
+
+    ngOnInit(): void {
+        this.paramSubscription = this.active.params.subscribe(u => {
+            const id = String(Object.values(u))
+            this.industrySubscription = this.industryService.getbyId(id).subscribe(result => {
+                this.industryForm.controls['id'].setValue(result.data.id)
+                this.industryForm.controls['industryCode'].setValue(result.data.industryCode)
+                this.industryForm.controls['industryName'].setValue(result.data.industryName)
+                this.industryForm.controls['isActive'].setValue(result.data.isActive)
+                this.industryForm.controls['version'].setValue(result.data.version)
+            })
+        })
+    }
+
+    clickUpdate(){
+        this.industryUpdateSubscription = this.industryService.update(this.industryForm.value).subscribe()
+    }
+
+    ngOnDestroy(): void {
+        this.industrySubscription?.unsubscribe()
+        this.industryUpdateSubscription?.unsubscribe()
+        this.paramSubscription?.unsubscribe()
+    }
+
+}

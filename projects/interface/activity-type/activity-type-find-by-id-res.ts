900cff6f1ce0f3a2af8f2c7c9d215f3a7c70b898
Angger Muhammad ELegant <anggerelegant7@gmail.com>
2022-11-28 10:29:14 +0700

    insert activity

--- projects/interface/activity-type/activity-type-find-by-id-res.ts
+++
@@ -1,6 +0,0 @@
-import { ActivityTypeData } from './activity-type-data' 
-
-export interface ActivityTypeFindByIdRes { 
-	 data: ActivityTypeData 
-} 
-
--- projects/memberarea/src/app/pages/activity/activity-insert/activity-insert.component.ts
+++ projects/memberarea/src/app/pages/activity/activity-insert/activity-insert.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormArray, FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { ActivityTypesRes } from "projects/interface/activity-type/activity-types-res";
+import { ActivityTypeService } from "projects/mainarea/src/app/service/activity-type.service";
 import { ActivityService } from "projects/mainarea/src/app/service/activity.service";
 import { FileService } from "projects/mainarea/src/app/service/file.service";
 import { Subscription } from "rxjs";
@@ -11,44 +13,59 @@ import { Subscription } from "rxjs";
 })
 export class ActivityInsertComponent implements OnInit, OnDestroy {
 
-    activityType: any = [
-        { name: "event" },
-        { name: "course" }
-    ]
-
     private activitySubscription?: Subscription
+    private activityTypesSubscription?: Subscription
+    activityTypesRes!: ActivityTypesRes
+    activityTypes: any[] = []
 
     activityForm = this.fb.group({
         activityTitle: [null, [Validators.required]],
-        activityContent: [null, [Validators.required]],
-        attachmentactivityInsertReqs: this.fb.array([])
+        activityLocation: [null, [Validators.required]],
+        provider: [null, [Validators.required]],
+        rangeDates: [null, [Validators.required]],
+        startAt: [null, [Validators.required]],
+        endAt: [null, [Validators.required]],
+        fee: [null, [Validators.required]],
+        activityTypeId: [null, [Validators.required]],
+        attachmentActivityInsertReqs: this.fb.array([])
     })
 
-    constructor(private activityService: ActivityService, private fb: FormBuilder,
+    constructor(private activityService: ActivityService, private activityTypeService: ActivityTypeService, private fb: FormBuilder,
         private router: Router, private fileService: FileService) { }
 
     ngOnInit(): void {
-
+        this.activityTypesSubscription = this.activityTypeService.getAll().subscribe(result => {
+            this.activityTypesRes = result
+            for (let i = 0; i < this.activityTypesRes.data.length; i++) {
+                this.activityTypes.push({
+                    name: this.activityTypesRes.data[i].activityTypeName,
+                    code: this.activityTypesRes.data[i].activityTypeCode,
+                    id: this.activityTypesRes.data[i].id
+                })
+            }
+        })
     }
 
     clickSubmit() {
+        this.activityForm.value.startAt = this.activityForm.value.rangeDates![0]
+        this.activityForm.value.endAt = this.activityForm.value.rangeDates![1]
         this.activitySubscription = this.activityService.insert(this.activityForm.value).subscribe(() => {
             this.router.navigateByUrl('/activitys/list')
         })
     }
     get detailFoto(): FormArray {
-        return this.activityForm.get('attachmentactivityInsertReqs') as FormArray
+        return this.activityForm.get('attachmentActivityInsertReqs') as FormArray
     }
 
     fileUpload(event: any) {
         this.fileService.fileUploadMulti(event).then(result => {
             this.detailFoto.push(this.fb.group({ extensions: result[0][0], fileCodes: result[0][1] }));
             console.log(this.activityForm.value);
-
         })
     }
 
     ngOnDestroy(): void {
         this.activitySubscription?.unsubscribe()
+        this.activityTypesSubscription?.unsubscribe()
     }
 }

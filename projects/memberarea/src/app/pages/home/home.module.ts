366780c16abb6368b44ca601e5ce0c2398d7bb3f
Ince Clara Mituduan <inceclaramituduan@gmail.com>
2022-11-28 10:46:46 +0700

    update table article admin(update button & add text overflow)

--- projects/memberarea/src/app/pages/home/home.module.ts
+++ projects/memberarea/src/app/pages/home/home.module.ts
@@ -18,6 +18,8 @@ import { CalendarModule } from 'primeng/calendar';
 import { TabViewModule } from 'primeng/tabview';
 import { ReactiveFormsModule } from "@angular/forms";
 import { rightContentComponent } from "./right-content/right-content.component";
+import { CheckboxModule } from 'primeng/checkbox';
+
 
 
 @NgModule({
@@ -41,7 +43,8 @@ import { rightContentComponent } from "./right-content/right-content.component";
         DividerModule,
         CalendarModule,
         TabViewModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        CheckboxModule
     ],
     exports: [
         HomeComponent, rightContentComponent
